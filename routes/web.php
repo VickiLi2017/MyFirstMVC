55e4c730af58bca7ed87f9b424144869d2a97efe
VickiLi2017 <vl54@njit.edu>
2017-10-25 17:48:06 -0400

    Finished Eloquent 101

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,23 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Task;
 
 Route::get('/tasks', function () {
 
-    $tasks = DB::table('tasks')->get();  //['Go to the store', 'Finish my screencast', 'Clean the house'];
-
+    //$tasks = DB::table('tasks')->get();  //['Go to the store', 'Finish my screencast', 'Clean the house'];
+    //$tasks = App\Task::all();
+    $tasks = Task::all();
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function($id){
-    $task = DB::table('tasks')->find($id);
+    //$task = DB::table('tasks')->find($id);
+    //$task =App\Task::find($id);
+
+    $task = Task::find($id);
+    //Task::incomplete();
+
     return view('tasks.show', compact('task'));
 });
 
